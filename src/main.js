12c251128477cf8440005aea091a7a1f20e0d629
lvingonzales <lvingonzales15@gmail.com>
2025-01-05 12:55:57 -0500

    Added cipher function, passed all tests, added analyzeArray function, passed all tests

--- src/main.js
+++ src/main.js
@@ -35,4 +35,102 @@ const calculator = {
     },
 }
 
-module.exports = {reverseString, capitalizeFirstLetter, calculator};
+const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+function cipher(str, shiftFactor) {
+
+    str = Array.from(str);
+    str = encrypt(str, shiftFactor);
+    return str.join('');
+}
+
+function encrypt(str, shiftFactor, buffer = []) {
+    if(!str.length) {return}
+
+    let currChar = str.shift();
+
+    if (!letters.includes(String(currChar).toLowerCase())){
+        buffer.push(currChar);
+    } else {
+        let index = letters.indexOf(String(currChar).toLowerCase());
+        let newPos = checkForOverflow(index, shiftFactor);
+
+        if(currChar === currChar.toUpperCase()) {
+            currChar = letters[newPos].toUpperCase();
+        } else {
+            currChar = letters[newPos];
+        }
+
+        buffer.push(currChar);
+    }
+
+    encrypt(str, shiftFactor, buffer);
+
+    return buffer;
+}
+
+function checkForOverflow(index, shiftFactor) {
+    let sum = calculator.sum(index, shiftFactor);
+
+    if(sum >= 26){
+        let adjustedSum = calculator.subtract(sum,(Math.floor(calculator.divide(sum, 26)) * 26))
+        return adjustedSum;
+    }
+
+    return sum;
+}
+
+function analyzeArray (arr, obj = {}) {
+
+    obj.average = average(arr);
+    obj.min = minimum(arr);
+    obj.max = maximum(arr);
+    obj.length = arr.length;
+
+    return obj; 
+}
+
+function minimum (arr) {
+    let min;
+
+    arr.forEach(element => {
+        if(min === undefined){min = element;}
+
+        if(element < min) {
+            min = element;
+        }
+    });
+
+    return min;
+}
+
+function maximum (arr) {
+    let max;
+
+    arr.forEach(element => {
+        if(max === undefined){max = element;}
+
+        if(element > max) {
+            max = element;
+        }
+    });
+
+    return max;
+}
+
+function average(arr) {
+    let average = 0;
+    let sum = 0;
+
+    arr.forEach(element => {
+        sum = calculator.sum(sum,element);
+    });
+
+    average = calculator.divide(sum,arr.length);
+
+    return average
+}
+
+cipher("!HI!", 1);
+
+module.exports = {reverseString, capitalizeFirstLetter, calculator, cipher, analyzeArray};
--- src/main.test.js
+++ src/main.test.js
@@ -1,34 +1,71 @@
-const {reverseString, capitalizeFirstLetter, calculator} = require('./main');
+const {reverseString, capitalizeFirstLetter, calculator, cipher, analyzeArray} = require('./main');
 
-// Reverse string test 
-test ('String "apple" becomes "elppa"', () => {
-    expect(reverseString("apple")).toBe("elppa");
+// // Reverse string test 
+// test ('String "apple" becomes "elppa"', () => {
+//     expect(reverseString("apple")).toBe("elppa");
+// })
+
+// // Capitalize first letter test
+// test ('String "this is a test" becomes "This is a test"', () => {
+//     expect(capitalizeFirstLetter("this is a test")).toBe("This is a test");
+// })
+
+// // Calculator Tests
+// test('Sums 1+1 to give 2', () => {
+//     expect(calculator.sum(1,1)).toBe(2);
+// })
+
+// test('multiplies 2*2 to give 4', () => {
+//     expect(calculator.multiply(2,2)).toBe(4);
+// })
+
+// test ('subtracts 5 - 2 to give 3', () => {
+//     expect(calculator.subtract(5,2)).toBe(3);
+// })
+
+// test ('dividing by zero(0) should throw an error', () => {
+//     expect(function () {
+//         calculator.divide(2,0)
+//     }).toThrow(new Error('Divide by zero(0) error'));
+// })
+
+// test ('dividing 10 / 2 to give 5', () => {
+//     expect(calculator.divide(10,2)).toBe(5);
+// })
+
+// // Cipher tests
+// test ('Entering "bruh" into the cipher with a shift factor of 1 gives "csvi"', () => {
+//     expect(cipher("bruh", 1)).toBe('csvi');
+// })
+
+// test ('Entering "xyz" into the cipher with a shift factor of 3 gives "abc"', () => {
+//     expect(cipher("xyz", 3)).toBe('abc');
+// })
+
+// test ('Entering "HeLLo" into the cipher with a shift factor of 3 gives "KhOOr"', () => {
+//     expect(cipher("HeLLo", 3)).toBe('KhOOr');
+// })
+
+// test ('Entering "Hello, World!" into the cipher with a shift factor of 3 gives "Khoor, Zroug!"', () => {
+//     expect(cipher("Hello, World!", 3)).toBe('Khoor, Zruog!');
+// })
+
+// Analyze array Tests
+
+const obj = analyzeArray([1,8,3,4,2,6])
+
+test ('Passing the array [1,8,3,4,2,6], returns an average of 4', () => {
+    expect(obj.average).toBe(4);
 })
 
-// Capitalize first letter test
-test ('String "this is a test" becomes "This is a test"', () => {
-    expect(capitalizeFirstLetter("this is a test")).toBe("This is a test");
+test ('Passing the array [1,8,3,4,2,6], returns an minimum value of 1', () => {
+    expect(obj.min).toBe(1);
 })
 
-// Calculator Tests
-test('Sums 1+1 to give 2', () => {
-    expect(calculator.sum(1,1)).toBe(2);
+test ('Passing the array [1,8,3,4,2,6], returns an maximum value of 8', () => {
+    expect(obj.max).toBe(8);
 })
 
-test('multiplies 2*2 to give 4', () => {
-    expect(calculator.multiply(2,2)).toBe(4);
-})
-
-test ('subtracts 5 - 2 to give 3', () => {
-    expect(calculator.subtract(5,2)).toBe(3);
-})
-
-test ('dividing by zero(0) should throw an error', () => {
-    expect(function () {
-        calculator.divide(2,0)
-    }).toThrow(new Error('Divide by zero(0) error'));
-})
-
-test ('dividing 10 / 2 to give 5', () => {
-    expect(calculator.divide(10,2)).toBe(5);
+test ('Passing the array [1,8,3,4,2,6], returns an length value of 6', () => {
+    expect(obj.length).toBe(6);
 })
